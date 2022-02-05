524f9e9ebb422dd3c3565a4284f6b4f8e54837d1
Jackman108 <arjun-pandav@yandex.ru>
2022-02-06 01:21:38 +0300

    Login connect API

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,23 +3,31 @@ import {Field, reduxForm} from "redux-form";
 
 import {required} from "../../utils/validators/validators";
 import {Input} from "../common/FormsControls/FormsControls";
+import {connect} from "react-redux";
+import {login} from "../../redux/auth-reducer";
+import {Redirect} from "react-router-dom";
+import style from "./../common/FormsControls/FormsControls.module.css"
 
 const LoginForm = (props) => {
     return (
         <form onSubmit={props.handleSubmit}>
             <div>
-                <Field placeholder={"Login"} name={"login"}
+                <Field placeholder={"Email"} name={"email"}
                        validate={[required]}
                        component={Input}/>
             </div>
             <div>
-                <Field placeholder={"Password"} name={"password"}
+                <Field placeholder={"Password"} name={"password"} type={"password"}
                        validate={[required]}
                        component={Input}/>
             </div>
             <div>
                 <Field component={Input} name={"rememberMe"} type={"checkbox"}/> remember me
             </div>
+            { props.error && <div className={style.formSummaryError}>
+                {props.error}
+            </div>}
+
             <div>
                 <button>Login</button>
             </div>
@@ -31,7 +39,11 @@ const LoginReduxForm = reduxForm({form: 'login'})(LoginForm);
 
 const Login = (props) => {
     const onSubmit = (formData) => {
-        console.log(formData);
+        props.login(formData.email, formData.password, formData.rememberMe);
+    }
+
+    if (props.isAuth) {
+        return <Redirect to={"/profile"} />
     }
     return <div>
         <h1>LOGIN</h1>
@@ -39,4 +51,8 @@ const Login = (props) => {
     </div>
 }
 
-export default Login;
+const mapStateToProps = (state) => ({
+    isAuth: state.auth.isAuth
+})
+
+export default connect(mapStateToProps, {login}) (Login);
--- src/redux/auth-reducer.js
+++ src/redux/auth-reducer.js
@@ -1,4 +1,5 @@
 import {authAPI} from "../api/api";
+import {stopSubmit} from "redux-form";
 
 const SET_USER_DATA = 'SET_USER_DATA';
 
@@ -16,8 +17,7 @@ const authReducer = (state = initialState, action) => {
 
             return {
                 ...state,
-                ...action.data,
-                isAuth: true
+               ...action.payload
             }
 
         default:
@@ -26,7 +26,8 @@ const authReducer = (state = initialState, action) => {
 }
 
 
-export const setAuthUserData = (userId, email, login) => ({type: SET_USER_DATA, data: {userId, email, login}  })
+export const setAuthUserData = (userId, email, login, isAuth) => ({type: SET_USER_DATA, payload:
+    {userId, email, login, isAuth}  })
 
 export const getAuthUserData = () => (dispatch) => {
 
@@ -34,7 +35,29 @@ export const getAuthUserData = () => (dispatch) => {
         .then(response => {
             if (response.data.resultCode === 0) {
                 let {id, login, email} = response.data.data;
-                dispatch(setAuthUserData(id, email, login));
+                dispatch(setAuthUserData(id, email, login, true));
+            }
+        });
+}
+export const login = (email, password, rememberMe) => (dispatch) => {
+
+    authAPI.login(email, password, rememberMe)
+        .then(response => {
+            if (response.data.resultCode === 0) {
+                dispatch(getAuthUserData())
+            } else {
+                let message = response.data.message.length > 0
+                    ? response.data.message[0] : "Some error";
+                dispatch(stopSubmit("login", {_error: message}))
+            }
+        });
+}
+export const logout = () => (dispatch) => {
+
+    authAPI.logout()
+        .then(response => {
+            if (response.data.resultCode === 0) {
+             dispatch(setAuthUserData(null, null, null, false));
             }
         });
 }
