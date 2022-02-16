76842515dc5de21a673f375754d980e054eef5ad
Jackman108 <arjun-pandav@yandex.ru>
2022-02-16 22:35:07 +0300

    Download ProfileImage

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,18 +2,27 @@ import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
+import userPhoto from "../../../assets/images/user.jpg"
 
-
-const ProfileInfo = ({profile, status, updateStatus}) => {
+const ProfileInfo = ({profile, status, updateStatus, isOwner, savePhoto}) => {
     if (!profile) {
         return <Preloader/>
+    }
+
+    const onMainPhotoSelected = (e) => {
+        if (e.target.files.length){
+            savePhoto(e.target.files[0])
+        }
     }
 
     return (
         <div>
             <div>
                 <div className={s.descriptionBlock}>
-                    <img src={profile.photos.large} alt='logo'/>
+                    <img src={profile.photos.large || userPhoto} className={s.mainPhoto} alt='logo'/>
+                    {isOwner && <input type={"file"} accept=".jpg, .jpeg, .png"
+                                       onChange={onMainPhotoSelected}
+                    className={s.buttonChange}/>}
                     <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
                 </div>
             </div>
